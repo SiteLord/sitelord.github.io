bd5c76681f4bf6b4a1c121f64a6cfc39c317baa8
SiteLorrd <SiteLord@yandex.ru>
2018-04-12 19:47:10 +0400

    inin

--- Veggie/script.js
+++ Veggie/script.js
@@ -1,40 +1,32 @@
 $(document).ready(function(){
 
 $('#d1').click(function() { 
-	if($("#dish1").css('display', 'none')) {
 		$(".menu_list ul li a:not('#d1')").css('border-bottom', 'none');
 		$("#d1").css('border-bottom', '1px solid #555');
-		$(".menu .container:not('#dish1')").css('display', 'none');
-		$("#dish1").css('display', 'block');
-	}	
+		$(".menu .container:not('#dish1')").hide(800);
+		$("#dish1").show(800);
 });
 
 $('#d2').click(function() { 
-	if($("#dish2").css('display', 'none')) {
 		$(".menu_list ul li a:not('#d2')").css('border-bottom', 'none');
 		$("#d2").css('border-bottom', '1px solid #555');
-		$(".menu .container:not('#dish2')").css('display', 'none');
-		$("#dish2").css('display', 'block');
-	}	
+		$(".menu .container:not('#dish2')").hide(800);
+		$("#dish2").show(800);
 }); 
 
 
 $('#d3').click(function() { 
-	if($("#dish3").css('display', 'none')) {
 		$(".menu_list ul li a:not('#d3')").css('border-bottom', 'none');
 		$("#d3").css('border-bottom', '1px solid #555');
-		$(".menu .container:not('#dish3')").css('display', 'none');
-		$("#dish3").css('display', 'block');
-	}	
+		$(".menu .container:not('#dish3')").hide(800);
+		$("#dish3").show(800);
 }); 
 
 $('#d4').click(function() { 
-	if($("#dish4").css('display', 'none')) {
 		$(".menu_list ul li a:not('#d4')").css('border-bottom', 'none');
 		$("#d4").css('border-bottom', '1px solid #555');
-		$(".menu .container:not('#dish4')").css('display', 'none');
-		$("#dish4").css('display', 'block');
-	}	
+		$(".menu .container:not('#dish4')").hide(800);
+		$("#dish4").show(800);
 }); 
 
 });
